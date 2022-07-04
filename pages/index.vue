9d19948aa631c375187ad5d579b4ea0323b8c1a2
edwardz8 <tolklorist@gmail.com>
2022-07-03 21:42:59 -0400

    initial commit

--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="w-full h-screen mx-auto">
-    <Form />
-    <Feature />
-  </div>
-</template>
--- server/api/subscribe.js
+++ server/api/subscribe.js
@@ -4,16 +4,19 @@ import { createClient } from "@supabase/supabase-js";
 export default async (req, res) => {
   const queryObject = url.parse(req.url, true).query;
   const { SUPABASE_URL, SUPABASE_KEY } = process.env;
+
   try {
     const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
     let response;
     const { email } = queryObject;
     if (email) {
-      const { data } = await supabase.from("Subscriber").upsert({
+      const { data, error } = await supabase.from("Subscriber").upsert({
         email: email,
       });
       response = data;
     }
+    
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(response));
     res.end();
